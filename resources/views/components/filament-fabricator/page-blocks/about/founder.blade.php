51046c65248f547af2c1bb2c5964090670af1d39
Ali Tezcan <alitezcan147@icloud.com>
2024-11-25 15:09:36 +0300

    mission tab fixed

--- resources/views/components/filament-fabricator/page-blocks/about/founder.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/about/founder.blade.php
@@ -51,17 +51,6 @@
                     </button>
                 </li>
             </ul>
-            <div class="tab-content" id="pills-tabContent">
-                <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-                    {{ $missionContent }}
-                </div>
-                <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
-                    {{ $visionContent }}
-                </div>
-                <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
-                    {{ $valuesContent }}
-                </div>
-            </div>
         </div>
     </div>
 </section>
